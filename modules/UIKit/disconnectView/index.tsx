cc5a1ca84349129419d1a6fc066d31c0f397097f
NazarenkoAnna2021 <93328568+NazarenkoAnna2021@users.noreply.github.com>
2022-10-19 15:11:06 +0300

    check is netinfo boolean

--- modules/UIKit/disconnectView/index.tsx
+++ modules/UIKit/disconnectView/index.tsx
@@ -1,19 +1,19 @@
 import { useNetInfo } from '@react-native-community/netinfo';
-import { observer } from 'mobx-react';
 import React, { FC, useMemo } from 'react'
 import { Text, View } from 'react-native';
 import { useUiContext } from '../../../src/UIProvider';
 import { getStyle } from './styles';
 
-export const DisconnectView: FC = observer(() => {
+export const DisconnectView: FC = () => {
     const { colors, t } = useUiContext();
     const styles = useMemo(() => getStyle(colors), [colors]);
     const netInfo = useNetInfo();
+    const isConnected = useMemo(() => netInfo.isConnected || typeof netInfo.isConnected !== 'boolean', [netInfo]);
 
-    return (!netInfo.isConnected
+    return (!isConnected
         ? <View style={styles.container}>
             <Text style={styles.text}>{t('lostConnection')}</Text>
         </View >
         : null
     );
-});
+};
