37b46cb7e7ee13a91f891e41a11111f9095af0eb
NazarenkoAnna2021 <93328568+NazarenkoAnna2021@users.noreply.github.com>
2022-10-19 14:28:28 +0300

    fix netInfo

--- modules/UIKit/disconnectView/index.tsx
+++ modules/UIKit/disconnectView/index.tsx
@@ -1,21 +1,16 @@
+import { useNetInfo } from '@react-native-community/netinfo';
 import { observer } from 'mobx-react';
-import React, { FC, useEffect, useMemo, useState } from 'react'
+import React, { FC, useMemo } from 'react'
 import { Text, View } from 'react-native';
-import { netInfoObserver } from '../../../libs/netInfo/netInfoObserver';
 import { useUiContext } from '../../../src/UIProvider';
 import { getStyle } from './styles';
 
 export const DisconnectView: FC = observer(() => {
     const { colors, t } = useUiContext();
     const styles = useMemo(() => getStyle(colors), [colors]);
-    const [isConnected, setIsConnected] = useState(false);
+    const netInfo = useNetInfo();
 
-    useEffect(() => {
-        netInfoObserver.subscribeNetInfo(setIsConnected);
-        return () => { netInfoObserver.unsubscribe() };
-    }, []);
-
-    return (!isConnected
+    return (!netInfo.isConnected
         ? <View style={styles.container}>
             <Text style={styles.text}>{t('lostConnection')}</Text>
         </View >
