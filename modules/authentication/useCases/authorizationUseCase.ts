b80b20858405ab916bfc0ec3029a9410228e5aab
NazarenkoAnna2021 <93328568+NazarenkoAnna2021@users.noreply.github.com>
2022-10-24 10:22:41 +0300

    fix naming

--- modules/authentication/useCases/authorizationUseCase.ts
+++ modules/authentication/useCases/authorizationUseCase.ts
@@ -4,20 +4,20 @@ import { userService } from "../../entities/user/UserService";
 
 const checkEmptiness = (login: string, code: string) => {
     if (!login && !code) {
-        return { error: 'Fill login and code inputs' };
+        return { error: 'fillLoginCode' };
     }
     if (!login && code) {
-        return { error: 'Fill login input' };
+        return { error: 'fillLogin' };
     }
     if (login && !code) {
-        return { error: 'Fill code input' };
+        return { error: 'fillCode' };
     }
     return null;
 }
 
 const authorizeUser = async (login: string, code: string) => {
     appStateModel.isLoading = true;
-    const result = await userService.authorize(login, code);
+    const result = await userService.authorizeStaticUsers(login, code);
     userModel.user = result.user;
     appStateModel.isLoading = false;
     return { error: result.error }
--- modules/entities/user/UserService.ts
+++ modules/entities/user/UserService.ts
@@ -12,22 +12,22 @@ class UserService {
         // private links: ILinks,
     ) { }
 
-    authorize = async (login: string, password: string): Promise<{ user: IUser | null, error: string | null }> => {
+    authorizeStaticUsers = async (login: string, password: string): Promise<{ user: IUser | null, error: string | null }> => {
         try {
             const users = require('../../../__mocks__/users.json');
-            const result = this.processingResponse(users, login, password);
+            const result = this.processingStaticUsers(users, login, password);
             if (!result) {
-                return { user: null, error: 'Incorrect login or code' };
+                return { user: null, error: 'incorrectLoginCode' };
             } else {
                 return { user: result, error: null };
             }
         } catch (error) {
-            console.warn('UserService -> authorize: ', error);
+            console.warn('UserService -> authorizeStaticUsers: ', error);
             return { user: null, error: 'Request error' };
         }
     }
 
-    private processingResponse = (users: { data: IDefaultUser[] }, login: string, password: string): IUser | null => {
+    private processingStaticUsers = (users: { data: IDefaultUser[] }, login: string, password: string): IUser | null => {
         const currentUser = users.data.find(item => item.login === login && item.password === password);
         if (currentUser) {
             return { id: currentUser.id, login: currentUser.login, name: currentUser.name };
