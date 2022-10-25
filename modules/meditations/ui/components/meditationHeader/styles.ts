77bf7953d25a1ee7752aab523f8888f0f8d4f516
NazarenkoAnna2021 <93328568+NazarenkoAnna2021@users.noreply.github.com>
2022-10-25 22:49:09 +0300

    add meditations header

---
+++ modules/meditations/ui/components/meditationHeader/styles.ts
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+import { IColors } from '../../../../../src/UIProvider/colorTheme';
+
+export const getStyle = (colors: IColors) => {
+    const styles = StyleSheet.create({
+        container: {
+            
+        },
+        image:{
+
+        },
+        title:{
+            
+        },
+        timeWrapper:{
+
+        },
+        timeText:{
+            
+        }
+    });
+    return styles;
+}
--- modules/meditations/ui/meditationDetailsView/index.tsx
+++ modules/meditations/ui/meditationDetailsView/index.tsx
@@ -1,22 +1,32 @@
 import React, { FC, useCallback, useMemo } from 'react';
 import { useUiContext } from '../../../../src/UIProvider';
-import { Text } from 'react-native';
 import { AppHeader } from '../../../UIKit/appHeader';
 import { ScreenContainer } from '../../../UIKit/screenContainer';
 import { getStyle } from './styles';
 import { ParamListBase, useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
+import { MeditationHeader } from '../components/meditationHeader';
+import { meditationModel } from '../../../entities/meditation/MeditationModel';
+import { observer } from 'mobx-react';
 
-export const MeditationDetailsView: FC = () => {
-    const { colors, t } = useUiContext();
+export const MeditationDetailsView: FC = observer(() => {
+    const { colors } = useUiContext();
     const styles = useMemo(() => getStyle(colors), [colors]);
     const navigation = useNavigation<StackNavigationProp<ParamListBase>>();
-
-    const onGoBack = useCallback(()=>navigation.navigate('MeditationsView'),[])
+    meditationModel.chosenMeditations = meditationModel.meditations?.[0] || null;
+    const onGoBack = useCallback(() => navigation.navigate('MeditationsView'), []);
 
     return (
-        <ScreenContainer containerStyle={styles.container} headerComponent={<AppHeader isBackButton={true} title={t('meditations')} onGoBack={onGoBack} />}>
-            <Text style={styles.text}>MeditationDetailsView</Text>
+        <ScreenContainer containerStyle={styles.container} headerComponent={<AppHeader isBackButton={true} onGoBack={onGoBack} />}>
+            <MeditationHeader
+                title={meditationModel.chosenMeditations?.title}
+                duration={meditationModel.chosenMeditations?.duration}
+                durationMeasuring={meditationModel.chosenMeditations?.durationMeasuring}
+            />
+            {/* 
+            <MeditationPlayer/>
+            <MeditationTasks/>
+             */}
         </ScreenContainer>
     )
-};
+});
