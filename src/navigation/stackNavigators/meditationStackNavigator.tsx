5a3bc8b03f964021cae9f99cf922759fd0c0fba5
NazarenkoAnna2021 <93328568+NazarenkoAnna2021@users.noreply.github.com>
2022-10-26 01:41:15 +0300

    Update meditationStackNavigator.tsx

--- src/navigation/stackNavigators/meditationStackNavigator.tsx
+++ src/navigation/stackNavigators/meditationStackNavigator.tsx
@@ -3,15 +3,16 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { observer } from 'mobx-react';
 import { MeditationsView } from '../../../modules/meditations/ui/meditationsView';
 import { MeditationDetailsView } from '../../../modules/meditations/ui/meditationDetailsView';
+import { IMeditation } from '../../../modules/entities/meditation/IMeditation';
 
 const Stack = createStackNavigator();
 
 export const MeditationStackNavigator: FC = observer(() => {
 
     return (
-        <Stack.Navigator initialRouteName='MeditationsView' screenOptions={{ headerShown: false, animationEnabled: false }} >
+        <Stack.Navigator initialRouteName='MeditationsView'  screenOptions={{ headerShown: false, animationEnabled: false }} >
             <Stack.Screen name='MeditationsView' component={MeditationsView} />
-            <Stack.Screen name='MeditationDetailsView' component={MeditationDetailsView} />
+            <Stack.Screen name='MeditationDetailsView' initialParams={{ item: null as IMeditation | null }} component={MeditationDetailsView} />
         </Stack.Navigator >
     );
 })
