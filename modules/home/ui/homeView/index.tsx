8d2c4980fa9c087abdcb2351103c13304a03e4a6
NazarenkoAnna2021 <93328568+NazarenkoAnna2021@users.noreply.github.com>
2022-10-25 12:50:28 +0300

    add banner logic

--- modules/home/ui/homeView/index.tsx
+++ modules/home/ui/homeView/index.tsx
@@ -1,20 +1,20 @@
 import React, { FC, useMemo } from 'react';
 import { useUiContext } from '../../../../src/UIProvider';
-import { IBanner } from '../../../entities/banner/IBanner';
+import { bannerModel } from '../../../entities/banner/BannerModel';
 import { AppHeader } from '../../../UIKit/appHeader';
 import { ScreenContainer } from '../../../UIKit/screenContainer';
+import { useHome } from '../../presenters/useHome';
 import { HomeBannersCarousel } from '../components/homeBannersCarousel';
 import { getStyle } from './styles';
-
-const MOCK_BANNERS: IBanner[] = require('../../../../__mocks__/banners.json').data;
 
 export const HomeView: FC = () => {
     const { colors, t } = useUiContext();
     const styles = useMemo(() => getStyle(colors), [colors]);
+    useHome();
 
     return (
         <ScreenContainer containerStyle={styles.container} headerComponent={<AppHeader title={t('startJourneyOneLine')} />} >
-            <HomeBannersCarousel banners={MOCK_BANNERS} />
+            <HomeBannersCarousel banners={bannerModel.bannersList || []} />
         </ScreenContainer>
     )
 };
