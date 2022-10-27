2c9eb1ca0ddce61815079e10b26d8065e7016307
NazarenkoAnna2021 <93328568+NazarenkoAnna2021@users.noreply.github.com>
2022-10-27 17:57:55 +0300

    add splash

--- src/navigation/rootNavigator/index.tsx
+++ src/navigation/rootNavigator/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useCallback } from 'react';
 import { KeyboardAvoidingView, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { AuthStackNavigator } from '../stackNavigators/authStackNavigator';
@@ -9,14 +9,17 @@ import { LoadingView } from '../../../modules/UIKit/loadingView';
 import { DisconnectView } from '../../../modules/UIKit/disconnectView';
 import { userModel } from '../../../modules/entities/user/UserModel';
 import { TabNavigator } from '../tabNavigator';
+import SplashScreen from 'react-native-splash-screen';
 
 export const RootNavigator: FC = observer(() => {
     const { colors, theme } = useUiContext();
 
+    const onReady = useCallback(() => setTimeout(() => SplashScreen.hide(), 1000), []);
+
     return (
         <KeyboardAvoidingView style={{ flex: 1, backgroundColor: colors.background }} behavior={Utils.isIOS ? 'padding' : undefined}>
             <StatusBar backgroundColor={colors.background} barStyle={theme === 'dark' ? 'light-content' : 'dark-content'} />
-            <NavigationContainer>
+            <NavigationContainer onReady={onReady}>
                 {!userModel.user
                     ? <AuthStackNavigator />
                     : <TabNavigator />
