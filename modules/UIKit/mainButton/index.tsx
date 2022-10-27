5c3861b2481d4417efc7cbcd0af6e0b09beb172b
NazarenkoAnna2021 <93328568+NazarenkoAnna2021@users.noreply.github.com>
2022-10-27 14:33:17 +0300

    Update main button

--- modules/UIKit/mainButton/index.tsx
+++ modules/UIKit/mainButton/index.tsx
@@ -6,13 +6,15 @@ import { getStyle } from './styles';
 
 interface Props {
     containerStyle?: ViewStyle;
+    colorEnd?: string;
+    colorStart?: string;
     title: string;
     onPress: () => void;
     disabled?: boolean;
     inProgress?: boolean;
 }
 
-export const MainButton: FC<Props> = memo(({ onPress = () => { }, title = '', disabled = false, inProgress = false, containerStyle = {} }) => {
+export const MainButton: FC<Props> = memo(({ onPress = () => { }, colorEnd, colorStart, title = '', disabled = false, inProgress = false, containerStyle = {} }) => {
     const { colors } = useUiContext();
     const styles = useMemo(() => getStyle(), []);
 
@@ -22,7 +24,7 @@ export const MainButton: FC<Props> = memo(({ onPress = () => { }, title = '', di
             style={({ pressed }) => [styles.container, containerStyle, { opacity: pressed || disabled ? 0.7 : 1 }]}
             onPress={onPress}
         >
-            <ButtonGradient colorEnd={colors.buttonGradientEnd} colorStart={colors.buttonGradientStart} title={title} />
+            <ButtonGradient colorEnd={colorEnd || colors.buttonGradientEnd} colorStart={colorStart || colors.buttonGradientStart} title={title} />
             {inProgress ? <View style={styles.absoluteSheet}><ActivityIndicator color={colors.regularText} size='large' /></View> : null}
         </Pressable>
     );
