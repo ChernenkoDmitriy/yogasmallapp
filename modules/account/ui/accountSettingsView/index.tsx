5a01ae107aea897a28b4306391e196fe2ff6c2f2
NazarenkoAnna2021 <93328568+NazarenkoAnna2021@users.noreply.github.com>
2022-10-28 10:17:08 +0300

    Update logout

--- modules/account/ui/accountSettingsView/index.tsx
+++ modules/account/ui/accountSettingsView/index.tsx
@@ -1,6 +1,9 @@
 import React, { FC, useCallback, useMemo } from 'react';
 import { View } from 'react-native';
 import { useUiContext } from '../../../../src/UIProvider';
+import { bannerModel } from '../../../entities/banner/BannerModel';
+import { meditationModel } from '../../../entities/meditation/MeditationModel';
+import { practiceModel } from '../../../entities/practice/PracticeModel';
 import { userModel } from '../../../entities/user/UserModel';
 import { MainButton } from '../../../UIKit/mainButton';
 import { getStyle } from './styles';
@@ -9,7 +12,12 @@ export const AccountSettingsView: FC = () => {
     const { colors, t } = useUiContext();
     const styles = useMemo(() => getStyle(colors), [colors]);
 
-    const onLogout = useCallback(() => { userModel.clear() }, []);
+    const onLogout = useCallback(() => { 
+        userModel.clear();
+        bannerModel.clear();
+        meditationModel.clear();
+        practiceModel.clear();
+    }, []);
 
     return (
         <View style={styles.container}>
