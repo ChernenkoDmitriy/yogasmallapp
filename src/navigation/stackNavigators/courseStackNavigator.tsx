6fbfaad92459018770c2d0863362a49cee2f7335
NazarenkoAnna2021 <93328568+NazarenkoAnna2021@users.noreply.github.com>
2022-10-31 14:01:05 +0200

    update navigation

--- src/navigation/stackNavigators/courseStackNavigator.tsx
+++ src/navigation/stackNavigators/courseStackNavigator.tsx
@@ -2,6 +2,9 @@ import React, { FC } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { observer } from 'mobx-react';
 import { CoursesView } from '../../../modules/courses/ui/coursesView';
+import { CoursesDaysView } from '../../../modules/courses/ui/coursesDaysView';
+import { IMeditation } from '../../../modules/entities/meditation/IMeditation';
+import { MeditationDetailsView } from '../../../modules/UIKit/meditationDetailsView';
 
 const Stack = createStackNavigator();
 
@@ -10,6 +13,8 @@ export const CourseStackNavigator: FC = observer(() => {
     return (
         <Stack.Navigator initialRouteName='CoursesView' screenOptions={{ headerShown: false }} >
             <Stack.Screen name='CoursesView' component={CoursesView} />
+            <Stack.Screen name='CoursesDaysView' component={CoursesDaysView} />
+            <Stack.Screen name='MeditationDetailsView' initialParams={{ item: null as IMeditation | null }} component={MeditationDetailsView} />
         </Stack.Navigator >
     );
 })
