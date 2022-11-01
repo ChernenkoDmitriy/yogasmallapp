25ff38d80ae2ad1b8a545e0194655ef579f4641d
NazarenkoAnna2021 <93328568+NazarenkoAnna2021@users.noreply.github.com>
2022-11-01 16:18:34 +0200

    create useAvailibility

---
+++ libs/hooks/useAvailability.ts
@@ -0,0 +1,27 @@
+import { useCallback, useMemo, useState } from 'react';
+import { IAccessCode } from '../../modules/entities/meditation/IAccessCode';
+
+export const useAvailability = (model: { accessCodes: IAccessCode[] }, accessCode: string, id: number) => {
+    const [code, setCode] = useState('');
+
+    const isAvailable = useMemo(() => {
+        if (model.accessCodes && accessCode) {
+            const match = model.accessCodes.find(item => item.meditationId === id && item.accessCode === accessCode);
+            return !!match;
+        } else {
+            return !accessCode;
+        }
+    }, [accessCode, id, model.accessCodes]);
+
+    const onGetAccess = useCallback(() => {
+        if (accessCode === code) {
+            model.accessCodes = [
+                ...model.accessCodes,
+                { meditationId: id, accessCode: code }
+            ]
+        };
+        setCode('');
+    }, [accessCode, code]);
+
+    return { code, setCode, isAvailable, onGetAccess };
+};
