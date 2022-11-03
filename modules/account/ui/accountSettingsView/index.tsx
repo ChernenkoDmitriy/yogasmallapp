9d361afbd95f06266fb3e1e8042b613d9c6930ec
NazarenkoAnna2021 <93328568+NazarenkoAnna2021@users.noreply.github.com>
2022-11-03 14:06:53 +0200

    apdate account

--- modules/account/ui/accountSettingsView/index.tsx
+++ modules/account/ui/accountSettingsView/index.tsx
@@ -1,20 +1,27 @@
 import { NavigationProp, ParamListBase, useNavigation } from '@react-navigation/native';
-import React, { FC, useCallback, useMemo } from 'react';
-import { View } from 'react-native';
+import { observer } from 'mobx-react';
+import React, { FC, useCallback, useMemo, useState } from 'react';
+import { Text, TouchableOpacity } from 'react-native';
 import { useUiContext } from '../../../../src/UIProvider';
 import { bannerModel } from '../../../entities/banner/BannerModel';
 import { meditationModel } from '../../../entities/meditation/MeditationModel';
 import { practiceModel } from '../../../entities/practice/PracticeModel';
 import { userModel } from '../../../entities/user/UserModel';
+import { AppHeader } from '../../../UIKit/appHeader';
 import { MainButton } from '../../../UIKit/mainButton';
+import { ScreenContainer } from '../../../UIKit/screenContainer';
+import { AccountLanguageModal } from '../components/accountLanguageModal';
 import { getStyle } from './styles';
 
-export const AccountSettingsView: FC = () => {
-    const { colors, t } = useUiContext();
+export const AccountSettingsView: FC = observer(() => {
+    const { colors, t, locale } = useUiContext();
     const styles = useMemo(() => getStyle(colors), [colors]);
     const navigation = useNavigation<NavigationProp<ParamListBase>>();
-    
-    const onLogout = useCallback(() => { 
+    const [isVisible, setIsVisible] = useState(false);
+
+    const onSetIsVisible = useCallback(() => { setIsVisible(prevState => !prevState) }, []);
+
+    const onLogout = useCallback(() => {
         navigation.navigate('HomeStackNavigator');
         userModel.clear();
         bannerModel.clear();
@@ -23,9 +30,13 @@ export const AccountSettingsView: FC = () => {
     }, []);
 
     return (
-        <View style={styles.container}>
-            <View></View>
+        <ScreenContainer containerStyle={styles.container} headerComponent={<AppHeader title={userModel.user?.name} />}>
+            <TouchableOpacity style={styles.settingsBlock} onPress={onSetIsVisible}>
+                <Text style={styles.settingsTitle}>{t('language')}</Text>
+                <Text style={styles.localeText}>{locale}</Text>
+            </TouchableOpacity>
             <MainButton title={t('logout')} colorEnd={colors.blockBackground} colorStart={colors.blockBackground} onPress={onLogout} containerStyle={styles.logoutButton} />
-        </View>
+            <AccountLanguageModal isVisible={isVisible} onClose={onSetIsVisible} />
+        </ScreenContainer>
     )
-};
+});
