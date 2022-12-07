7b9d8f9fcb450a6ab03712024b7b5705a2b324c9
NazarenkoAnna2021 <annanazarenko1234@gmail.com>
2022-12-07 17:41:55 +0200

    remake codes to array

--- libs/hooks/useAvailability.ts
+++ libs/hooks/useAvailability.ts
@@ -1,24 +1,28 @@
 import { useCallback, useMemo, useState } from 'react';
 import { IAccessCode } from '../../modules/entities/meditation/IAccessCode';
 
-export const useAvailability = (model: { accessCodes: IAccessCode[] }, accessCode: string, id: number) => {
+export const useAvailability = (model: { accessCodes: IAccessCode[] }, accessCode: string[], id: number) => {
     const [code, setCode] = useState('');
 
     const isAvailable = useMemo(() => {
-        if (model.accessCodes && accessCode) {
-            const match = model.accessCodes.find(item => item.meditationId === id && item.accessCode === accessCode);
+        if (model.accessCodes && !!accessCode?.length) {
+            console.log('accessCode 1');
+            const match = model.accessCodes.find(item => item.meditationId === id && accessCode.includes(item.accessCode));
             return !!match;
         } else {
-            return !accessCode;
+            console.log('accessCode 2');
+            return !accessCode?.length;
         }
     }, [accessCode, id, model.accessCodes]);
 
     const onGetAccess = useCallback(() => {
-        if (accessCode === code) {
+        if (accessCode.includes(code)) {            
             model.accessCodes = [
                 ...model.accessCodes,
                 { meditationId: id, accessCode: code }
             ];
+            
+            // console.log(model.accessCodes );
             setCode('');
             return true;
         } else {
